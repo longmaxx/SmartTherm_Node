eba0be322b969e5da79f6db996eec48ce710a579
longmaxx <longmaxx@yandex.ru>
2018-01-07 20:21:16 +0300

    spiffs_1

--- SmartTherm_Node.ino
+++ SmartTherm_Node.ino
@@ -10,15 +10,13 @@
 #include <ESP8266WebServer.h>
 //#include <ESP8266mDNS.h>
 #include <ESP8266WiFiMulti.h>
-
 #include <WiFiUdp.h>
+
+#include <FS.h>
+File fsUploadFile;
 
 SimpleDHT22 Dht;
 ESP8266WiFiMulti WiFiMulti;
-
-#include "Page_Root.h"
-#include "Page_SetName.h"
-#include "Page_SetDate.h"
 
 /*
 4 (D2)  - RTC
@@ -46,11 +44,37 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP,3*3600);
 bool flag_TimeIsOK = false;
 
+#define DBG_OUTPUT_PORT Serial
+
+//format bytes
+String formatBytes(size_t bytes) {
+  if (bytes < 1024) {
+    return String(bytes) + "B";
+  } else if (bytes < (1024 * 1024)) {
+    return String(bytes / 1024.0) + "KB";
+  } else if (bytes < (1024 * 1024 * 1024)) {
+    return String(bytes / 1024.0 / 1024.0) + "MB";
+  } else {
+    return String(bytes / 1024.0 / 1024.0 / 1024.0) + "GB";
+  }
+} 
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("");
   Serial.println("Setup...");
+  SPIFFS.begin();
+  {
+    Dir dir = SPIFFS.openDir("/");
+    while (dir.next()) {
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      DBG_OUTPUT_PORT.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
+    }
+    DBG_OUTPUT_PORT.printf("\n");
+  }
+  
   initDS18B20();
   initWifi();
   initWebServer();
@@ -207,7 +231,7 @@ String getNowTimeStr()
 {
   if ( flag_TimeIsOK )
   {
-    return "<time error>";
+    return "=time error=";
   }
   else
   {
@@ -245,79 +269,182 @@ String firstZero(int val)
     return (String)val;
   }
 }
+
+//===================================== SPIFFS==============================================================
+String getContentType(String filename) {
+  if (server.hasArg("download")) return "application/octet-stream";
+  else if (filename.endsWith(".htm")) return "text/html";
+  else if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".png")) return "image/png";
+  else if (filename.endsWith(".gif")) return "image/gif";
+  else if (filename.endsWith(".jpg")) return "image/jpeg";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".xml")) return "text/xml";
+  else if (filename.endsWith(".pdf")) return "application/x-pdf";
+  else if (filename.endsWith(".zip")) return "application/x-zip";
+  else if (filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+bool handleFileRead(String path) {
+  DBG_OUTPUT_PORT.println("handleFileRead: " + path);
+  if (path.endsWith("/")) path += "index.htm";
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) {
+    if (SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
 //=====================================Web Server===========================================================
 void initWebServer()
 {
   //if ( MDNS.begin ( "esp8266" ) ) {
   //  Serial.println ( "MDNS responder started" );
   //}
-  server.on ( "/", handleRoot );
-  server.on ( "/setname", handleSetName );
-  server.on ( "/setdate", handleSetDate );
+  //server.on ( "/", handleRoot );
+  //server.on ( "/setname", handleSetName );
+  //server.on ( "/setdate", handleSetDate );
+  server.on("/last",HTTP_GET, handleAjaxDataGet);
+    //SERVER INIT
+  //list directory
+  server.on("/list", HTTP_GET, handleFileList);
+  //load editor
+  server.on("/edit", HTTP_GET, []() {
+    if (!handleFileRead("/edit.htm")) server.send(404, "text/plain", "FileNotFound");
+  });
+  //create file
+  server.on("/edit", HTTP_PUT, handleFileCreate);
+  //delete file
+  server.on("/edit", HTTP_DELETE, handleFileDelete);
+  //first callback is called after the request has ended with all parsed arguments
+  //second callback handles file uploads at that location
+  server.on("/edit", HTTP_POST, []() {
+    server.send(200, "text/plain", "");
+  }, handleFileUpload);
   server.onNotFound ( handleNotFound );
   server.begin();  
 }
 
-void handleRoot() 
+void handleAjaxDataGet()
 {
-  String data = ROOT_page;
-  data.replace("@@DEVNAME@@", DeviceName);
-  data.replace("@@CURDATE@@", getNowTimeStr());
-  data.replace("@@LDATE@@", getFormattedTime(lastSensorData.Timestamp));
-  data.replace("@@LCELSIUM@@", (String)lastSensorData.Celsium);
-  data.replace("@@LHUMIDITY@@", (String)lastSensorData.Humidity);
-  server.send ( 200, "text/html; charset=utf-8", data );
-}
+  String data = "{";
 
-void handleSetName()
-{
-  String data = SETNAME_page;
-  if (server.method() == HTTP_POST){
-    // form submit
-    String newName = server.arg("devname");
-    if (newName != ""){
-      DeviceName = newName;
-    }
-    data.replace("@@RESULT@@", "Data were saved.");
-  }
-  data.replace("@@RESULT@@", "");
-  data.replace("@@DEVNAME@@", DeviceName);
-  server.send ( 200, "text/html; charset=utf-8", data );
-}
+   data +="\"devname\":\"" + DeviceName + "\"" + ",";
+   data +="\"celsium\":\"" + (String)lastSensorData.Celsium + "\",";
+   data +="\"humidity\":\"" + (String)lastSensorData.Humidity + "\",";
+   data +="\"date_now\":\"" + getNowTimeStr() + "\",";
+   data +="\"date_last\":\"" + getFormattedTime(lastSensorData.Timestamp) + "\"";
 
-void handleSetDate()
-{
-  String data = SETDATE_page;
-  if (server.method() == HTTP_POST){
-    RTC1.setDate(server.arg("day").toInt(), server.arg("month").toInt(), server.arg("year").toInt());
-    RTC1.setTime(server.arg("hour").toInt(), server.arg("minute").toInt(),0);
-    data.replace("@@RESULT@@", "Date was set.");
-  }
-  Time t = RTC1.getTime();
-  data.replace("@@RESULT@@", "");
-  data.replace("@@YEAR@@", (String)t.year);
-  data.replace("@@MONTH@@", (String)t.mon);
-  data.replace("@@DAY@@", (String)t.date);
-  data.replace("@@HOUR@@", (String)t.hour);
-  data.replace("@@MINUTE@@", (String)t.min);
-  
-  server.send ( 200, "text/html; charset=utf-8", data );
+   data +="}";
+    DBG_OUTPUT_PORT.println(data);
+   server.send(200,"application/json",data);
 }
 
 void handleNotFound() 
 {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
+  if (!handleFileRead(server.uri())){
+    String message = "File Not Found\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
+  
+    for ( uint8_t i = 0; i < server.args(); i++ ) {
+      message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+    }
 
-  for ( uint8_t i = 0; i < server.args(); i++ ) {
-    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+    server.send ( 404, "text/plain", message );
+  }  
+}
+
+//============================================web spiffs===================================================
+void handleFileUpload() {
+  if (server.uri() != "/edit") return;
+  HTTPUpload& upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START) {
+    String filename = upload.filename;
+    if (!filename.startsWith("/")) filename = "/" + filename;
+    DBG_OUTPUT_PORT.print("handleFileUpload Name: "); DBG_OUTPUT_PORT.println(filename);
+    fsUploadFile = SPIFFS.open(filename, "w");
+    filename = String();
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    //DBG_OUTPUT_PORT.print("handleFileUpload Data: "); DBG_OUTPUT_PORT.println(upload.currentSize);
+    if (fsUploadFile)
+      fsUploadFile.write(upload.buf, upload.currentSize);
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (fsUploadFile)
+      fsUploadFile.close();
+    DBG_OUTPUT_PORT.print("handleFileUpload Size: "); DBG_OUTPUT_PORT.println(upload.totalSize);
+  }
+}
+
+void handleFileDelete() {
+  if (server.args() == 0) return server.send(500, "text/plain", "BAD ARGS");
+  String path = server.arg(0);
+  DBG_OUTPUT_PORT.println("handleFileDelete: " + path);
+  if (path == "/")
+    return server.send(500, "text/plain", "BAD PATH");
+  if (!SPIFFS.exists(path))
+    return server.send(404, "text/plain", "FileNotFound");
+  SPIFFS.remove(path);
+  server.send(200, "text/plain", "");
+  path = String();
+}
+
+void handleFileCreate() {
+  if (server.args() == 0)
+    return server.send(500, "text/plain", "BAD ARGS");
+  String path = server.arg(0);
+  DBG_OUTPUT_PORT.println("handleFileCreate: " + path);
+  if (path == "/")
+    return server.send(500, "text/plain", "BAD PATH");
+  if (SPIFFS.exists(path))
+    return server.send(500, "text/plain", "FILE EXISTS");
+  File file = SPIFFS.open(path, "w");
+  if (file)
+    file.close();
+  else
+    return server.send(500, "text/plain", "CREATE FAILED");
+  server.send(200, "text/plain", "");
+  path = String();
+}
+
+void handleFileList() {
+  if (!server.hasArg("dir")) {
+    server.send(500, "text/plain", "BAD ARGS");
+    return;
   }
 
-  server.send ( 404, "text/plain", message );
+  String path = server.arg("dir");
+  DBG_OUTPUT_PORT.println("handleFileList: " + path);
+  Dir dir = SPIFFS.openDir(path);
+  path = String();
+
+  String output = "[";
+  while (dir.next()) {
+    File entry = dir.openFile("r");
+    if (output != "[") output += ',';
+    bool isDir = false;
+    output += "{\"type\":\"";
+    output += (isDir) ? "dir" : "file";
+    output += "\",\"name\":\"";
+    output += String(entry.name()).substring(1);
+    output += "\"}";
+    entry.close();
+  }
+
+  output += "]";
+  server.send(200, "text/json", output);
 }
